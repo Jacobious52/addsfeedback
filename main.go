d905e5bc4029c7014138e0723a569e5c4e1ee8bd
Jacobious52 <jacobgonzalez5252@hotmail.com>
2017-09-06 21:10:03 +0930

    + added saving to gist

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"crypto/subtle"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/Jacobious52/addsfeedback/app/controllers"
 	"github.com/Jacobious52/addsfeedback/app/models"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func BasicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
@@ -35,10 +40,35 @@ func main() {
 		port = "3000"
 	}
 
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Fatalln("No Gist Token specified. Add env var $TOKEN")
+	}
+
 	rand.Seed(time.Now().Unix())
 
+	// setup gist api
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	gist := models.LoadStats(ctx, client)
+
 	models.LoadDatabase("db/feedback.json")
-	models.LoadStats("db/stats.json")
+
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-signalChannel
+		switch sig {
+		case syscall.SIGINT, syscall.SIGTERM:
+			models.SaveStats(ctx, client, gist)
+			os.Exit(0)
+		}
+	}()
 
 	// routes
 	fs := http.FileServer(http.Dir("static"))
@@ -51,5 +81,9 @@ func main() {
 	http.HandleFunc("/feedback", BasicAuth(controllers.Feedback, "addsmarker", "c++11", "addsmarkersite"))
 	http.HandleFunc("/stats", BasicAuth(controllers.Statistics, "addsmarker", "c++11", "addsmarkersite"))
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	models.SaveStats(ctx, client, gist)
 }
